403f849c0055052ec0d8193b1e9a80bc6380ef49
mati <matee803@gmail.com>
2023-10-01 18:27:12 +0600

    first commit added

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text7xl bg-rose-600'>Vite + React</h1>
+      
+
+    </>
+  )
+}
+
+export default App
